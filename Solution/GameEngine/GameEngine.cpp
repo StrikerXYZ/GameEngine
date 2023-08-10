fdfdf61a79ab2f9fc57d976857f50f3837ac6db6
StrikerXYZ <xxwebmasterxx2html@gmail.com>
2023-08-10 20:53:08 +1000

    Setup clang and WinMain

---
+++ Solution/GameEngine/GameEngine.cpp
@@ -0,0 +1,18 @@
+﻿// GameEngine.cpp : Defines the entry point for the application.
+//
+
+#include <windows.h>
+#include "GameEngine.h"
+
+using namespace std;
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow
+)
+{
+	OutputDebugStringA("This is a test\n");
+	return 0;
+}

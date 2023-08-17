39e248b7a98d11d4166c596b3d6859ffd98add06
StrikerXYZ <xxwebmasterxx2html@gmail.com>
2023-08-17 22:44:48 +1000

    Setup a message box

---
+++ Solution/Source/GameEngine.cpp
@@ -0,0 +1,13 @@
+﻿// GameEngine.cpp : Defines the entry point for the application.
+//
+
+#include "GameEngine.h"
+
+#include <windows.h>
+
+
+int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(0, "Hello!", "Engine", MB_OK | MB_ICONINFORMATION);
+	return 0;
+}

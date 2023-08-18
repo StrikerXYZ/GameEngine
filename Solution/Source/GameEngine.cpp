f8a4e49c93dd7e9a3752b3c2f6842ea284bc2ca6
StrikerXYZ <xxwebmasterxx2html@gmail.com>
2023-08-18 22:48:58 +1000

    show window

--- Solution/Source/GameEngine.cpp
+++ Solution/Source/GameEngine.cpp
@@ -5,9 +5,106 @@
 
 #include <windows.h>
 
+LRESULT CALLBACK WindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+	LRESULT Result = 0;
 
-int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugString("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugString("WM_DESTROY\n");
+		} break;
+
+		case WM_CLOSE:
+		{
+			OutputDebugString("WM_CLOSE\n");
+		} break;
+
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugString("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+
+	return Result;
+}
+
+int CALLBACK WinMain( HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode)
 {
 	MessageBox(0, "Hello!", "Engine", MB_OK | MB_ICONINFORMATION);
+
+	WNDCLASS WindowClass = {}; 
+	WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+	WindowClass.lpfnWndProc = WindowCallback;
+	WindowClass.hInstance = Instance;
+	WindowClass.lpszClassName = "GameEngineClass";
+	
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowEx(
+			0,
+			WindowClass.lpszClassName,
+			"Game Engine",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+			0
+		);
+
+		if (WindowHandle)
+		{
+			MSG Message;
+			for (;;)
+			{
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if(MessageResult > 0)
+				{
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			//todo: log
+		}
+	}
+	else
+	{
+		//todo: log
+	}
+
 	return 0;
 }
